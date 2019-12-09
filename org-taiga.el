72da5c12ccfdf219ee67a975a6ca3e4c17be0b82
Konstantin Sorokin <sorokin.kc@gmail.com>
2019-12-09 21:33:33 +0300

    Add main file stub

---
+++ org-taiga.el
@@ -0,0 +1,35 @@
+;;; org-taiga.el --- Integration between taiga project management and emacs org-mode -*- lexical-binding: t -*-
+
+;; Copyright (c) 2019 Konstantin Sorokin (GNU/GPL Licence)
+
+;; Authors: Konstantin Sorokin <sorokin.kc@gmail.com>
+;; URL: http://github.com/xmagpie/org-taiga
+;; Version: 0.1.0
+;; Package-Requires: ((emacs "26.2"))
+;; Keywords: tools, org-mode, taiga, project-management
+
+;; This file is NOT part of GNU Emacs.
+
+;; This application is free software: you can redistribute it and/or modify
+;; it under the terms of the GNU General Public License as published by
+;; the Free Software Foundation, either version 3 of the License, or
+;; (at your option) any later version.
+
+;; This application is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with this application.
+;; If not, see <http://www.gnu.org/licenses/>.
+
+;;; Commentary:
+;;;
+;;;
+;;;
+
+;;; Code:
+
+(provide 'org-taiga)
+;;; org-taiga.el ends here
